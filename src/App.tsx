0b3630bfcda3b80f2a23f0b9a6d3ca4dfc0d87fc
habibHadjar <lhp6.hhadjar@gmail.com>
2022-04-04 16:41:31 +0200

    fix import img files + Hero

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss'
 import Nav from './components/Nav/Nav'
+import Hero from './components/Hero/Hero'
 
 export default function App() {
 	return (
 		<>
 			<Nav />
+			<Hero />
 		</>
 	)
 }
