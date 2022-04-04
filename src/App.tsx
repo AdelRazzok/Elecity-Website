d6ac7742bea47d97be1f7627734ad85954de6317
PaulGimay <81556930+PaulGimay@users.noreply.github.com>
2022-04-04 16:45:09 +0200

    Footer Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.scss'
+import Footer from '../src/components/Footer/Footer'
+import Map from '../src/components/Map/Map'
 
 export default function App() {
 	return (
 		<div className="App">
 			<h1>Elecity</h1>
+			<Map />
+			<Footer />
 		</div>
 	)
 }
---
+++ src/components/Map/Map.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Map.scss';
+
+
+
+
+
+const Map = () => {
+    return (
+        <div className="Map">
+        <div className="Map-display">
+        </div>
+        </div>
+    )
+}
+
+export default Map
