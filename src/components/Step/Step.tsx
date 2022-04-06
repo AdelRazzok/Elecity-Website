2a5a3bd05a9d8a0ec1e78829ce1004e55d1b8092
= <LHP6.arazzok@gmail.com>
2022-04-06 14:37:07 +0200

    add Steps and style

--- src/components/Step/Step.tsx
+++ src/components/Step/Step.tsx
@@ -1,9 +1,18 @@
-import './Introduction.scss'
+import './Step.scss'
 
-const Step: React.FC = () => {
+interface Props {
+	id: number
+	text: string
+}
+
+const Step: React.FC<Props> = ({ id, text }) => {
   return (
 	<div className='Step'>
-
+		<div className="Step-wrapper">
+			<p>{text}</p>
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, porro.</p>
+		</div>
+		<span>{id}.</span>
 	</div>
   )
 }
