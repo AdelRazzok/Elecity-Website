10835cf8f03115c09e40b16f6cca08a306068372
habibHadjar <lhp6.hhadjar@gmail.com>
2022-04-06 10:27:40 +0200

    light font >

--- src/components/HeroOffer/HeroOffer.tsx
+++ src/components/HeroOffer/HeroOffer.tsx
@@ -12,49 +12,36 @@ interface Props {
   toggleActiveOnOffer: (id: number) => void;
 }
 
-const HeroOffer: React.FC<Props> = ({offer, toggleActiveOnOffer}: Props) => {
-  
+const HeroOffer: React.FC<Props> = ({ offer, toggleActiveOnOffer }: Props) => {
+
   const handleClick = () => {
     toggleActiveOnOffer(offer.id)
   }
-  
+
   return (
     <>
-      {/* <div key={offer.id} className="HeroOffer">
-        <div className="HeroOffer-img">
-          <img src={offer.img} alt="" />
-        </div>
-        <div className="HeroOffer-info">
-          <h2>{offer.type}</h2>
-          <h3>{offer.model}</h3>
-          <p>{offer.traits.join(' • ')}</p>
-          <h4>{offer.price}€/heure</h4>
-        </div>
-      </div> */}
-
       <div onClick={handleClick} className={`HeroOffer ${offer.active ? 'active' : ''}`} >
 
-        {offer.active &&
-        <div className="HeroOffer-info">
-          
-          <div className="HeroOffer-info-head">
-            <img src={brandLogo} alt="" />
-            <span>PEUGEOT - {offer.type}</span>
-            <img src={plug} alt="" />
-          </div>
-
-          <h3>{offer.model}</h3>
-
-          <p>{offer.traits.join(' • ')}</p>
-          
-          <h4>{offer.price}€/heure</h4>
-
-          <Button link={'#'} text={'Louer'}/>
-
+        <div className="HeroOffer-head">
+          {offer.active && <img src={brandLogo} />}
+          <span>
+            {offer.active && <span>PEUGEOT</span>}
+            {offer.active && <span>-</span>}
+            <span>{offer.type}</span>
+          </span>
+          <img src={plug} />
         </div>
-        }
-        
-        <img className="HeroOffer-carImg" src={citadine} />
+
+        <div className="HeroOffer-body">
+          <div className="HeroOffer-body-info">
+            {offer.active && <h3>{offer.model}</h3>}
+            {offer.active && <p>{offer.traits.join(' • ')}</p>}
+            {offer.active && <h3>{offer.price}€/heure</h3>}
+            {offer.active && <Button link='#' text='Louer' />}
+          </div>
+          <img src={citadine} />
+        </div>
+        {!offer.active && <span>{offer.model}</span>}
 
       </div>
     </>
