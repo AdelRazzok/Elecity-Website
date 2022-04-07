73bbb9e4867a14c0e45d2a694d028ab3e1b0c13b
= <LHP6.arazzok@gmail.com>
2022-04-07 11:27:51 +0200

    add styles props to Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,15 @@
+import { CSSProperties } from 'react'
 import './Button.scss'
 
 interface Props {
 	link: string
 	text: string
+	styles: CSSProperties
 } 
 
-const Button: React.FC<Props> = ({ link, text }) => {
-	return <a href={link} className='Button'>{text}</a>
+const Button: React.FC<Props> = ({ link, text, styles }) => {
+	return (
+		<a href={link} className='Button' style={styles}>{text}</a>
+	)
 } 
 export default Button
