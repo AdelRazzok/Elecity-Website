077ed315e1349a6d706d54280c17945ae0a1dc8b
= <LHP6.arazzok@gmail.com>
2022-04-08 12:08:18 +0200

    update steps titles and texts

--- src/components/Steps/stepsData.js
+++ src/components/Steps/stepsData.js
@@ -1,20 +1,25 @@
+import signUpStep from '../../assets/img/signUpStep.png'
+import offerStep from '../../assets/img/offerStep.png'
+import moveStep from '../../assets/img/moveStep.png'
+
+
 export default [
 	{
 		id: 1,
 		title: "Adhérer",
-		text: "Inscrivez-vous depuis le site ou l'appli mobile.",
-		svg: '#'
+		text: "Inscrivez-vous depuis le site web ou l'application mobile.",
+		svg: signUpStep,
 	},
 	{
 		id: 2,
-		title: "Selectionner",
-		text: "Choisissez l'offre qui vous convient.",
-		svg: '#'
+		title: "Sélectionner",
+		text: "Choisissez l'offre qui vous convient le mieux.",
+		svg: offerStep,
 	},
 	{
 		id: 3,
 		title: "Bouger",
 		text: "Rendez-vous au parc le plus proche de vous et profitez de votre véhicule 100% électrique.",
-		svg: '#'
+		svg: moveStep,
 	},
 ]
